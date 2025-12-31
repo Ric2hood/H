9f28983b63cff403595b5ebaf63be0ba7b5f0326
HENRIS <117238264+Ric2hood@users.noreply.github.com>
2025-12-30 21:01:16 -0500

    Actualizar server.js

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // ---------- CLOUDINARY CONFIG ----------
+console.log('Cloudinary ENV:', {
+  name: process.env.CLOUDINARY_CLOUD_NAME,
+  key: process.env.CLOUDINARY_API_KEY ? 'OK' : 'MISSING',
+  secret: process.env.CLOUDINARY_API_SECRET ? 'OK' : 'MISSING'
+}); 
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
